873f24a7bd1ed3de23599ad78f96cf11eef47a1b
kldxbjz <wzczsgdsg@gmail.com>
2021-12-10 02:50:07 -0500

    Initiated the main.cc file

--- main.cc
+++ main.cc
@@ -1,19 +1,14 @@
-// // Kunling Yang, 20912628
-// // Last Modified At 1929,20211201
+#include <iostream>
+#include <memory>
+#include "game.h"
+#include "human.h"
 
-// #include <iostream>
-// #include <memory>
-
-// int main() {
-//     std::string input;
-
-//     while(std::cin >> input) {
-//         if(std::cin.fail() || input == "quit") {
-//             break;
-//         }   // end if
-//         try {
-//         }   catch (std::runtime_error& e) {
-//             std::cout << e.what() << std::endl;
-//         }
-//     }
-// }   // end main
+int main() {
+    std::vector<std::unique_ptr<Player>> players;
+    players.emplace_back( std::make_unique<Human>( 1 ) );
+    players.emplace_back( std::make_unique<Human>( 2 ) );
+    std::vector<Player *> playerOfTheGame;
+    playerOfTheGame.emplace_back( players[0].get() );
+    playerOfTheGame.emplace_back( players[1].get() );
+    std::unique_ptr<Game> g = std::make_unique<Game>( playerOfTheGame );
+}
--- test.cc
+++ test.cc
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int a;
-    int b;
-    cin>>a>>b;
-    cout<<a+b;
-    return 0;
-}
+// int main() {
+//     int a;
+//     int b;
+//     cin>>a>>b;
+//     cout<<a+b;
+//     return 0;
+// }
